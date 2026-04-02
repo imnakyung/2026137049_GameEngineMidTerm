1a9ecec8b4da6a81c469fb4d42b41792718208ba
imnakyung <imnakyung1005@gmail.com>
2026-04-02 15:39:24 +0900

    2D 플랫포머 게임 제작 1

---
+++ 2026137049_GameEngineMidTerm/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private Animator pAni;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pAni = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        moveInput = input.x;
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("Jump");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (collision.CompareTag("Finish"))
+        {
+            collision.GetComponent<Level_scripts>().MoveToNextLevel();
+        }
+
+        if (collision.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
